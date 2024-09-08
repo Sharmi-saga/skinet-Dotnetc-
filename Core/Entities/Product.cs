1b20ed8f5c1043d2bf1c1f8973210efa68b7cd7f
Sharmi-saga <sharmitha2332@gmail.com>
2024-09-08 16:16:31 +0530

    Add : Adding soorting to the specification

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -6,7 +6,7 @@ namespace Core.Entities;
 public class Product : BaseEntity
 {
 
-  public required string name { get; set; }
+  public required string Name { get; set; }
   public required string Description { get; set; }
 
   public decimal Price { get; set;}
