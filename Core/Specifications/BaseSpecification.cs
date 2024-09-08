467fac5bda34f27e23e385aa3f0ae51d6a4a994d
Sharmi-saga <sharmitha2332@gmail.com>
2024-09-08 15:01:37 +0530

    Feat : Using Specification Pattern

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -7,6 +7,7 @@ namespace Core.Specifications;
 
 public class BaseSpecification<T>(Expression<Func<T, bool>> criteria): ISpecification<T>
 {
+    protected BaseSpecification () : this(null){}
 
-    public Expression<Func<T, bool>> Criteria => criteria;
+    public Expression<Func<T, bool>>? Criteria => criteria;
 }
